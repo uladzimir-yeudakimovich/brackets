739b5dd9c5963d486b4ee65a0bce2cdc18733b53
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-10 13:27:43 +0300

    80%

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  let count = 0;
+  const arr = [];
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
-      if (count < 0) return false;
-      if (str[i] === bracketsConfig[j][0]) {
-        count += 1;
-      } else if (str[i] === bracketsConfig[j][1]) {
-        count -= 1;
+      if (str[i] === bracketsConfig[j][0] && arr[arr.length-1] !== bracketsConfig[j][0]) {
+        arr.push(str[i]);
+      } else if (str[i] === bracketsConfig[j][1] && arr[arr.length-1] === bracketsConfig[j][0]) {
+        arr.pop();
       }
     }
   }
-  return count === 0;
+  return !arr.length;
 }
