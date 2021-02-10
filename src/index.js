a1bd094cb021e8c0ecc6bfec66b2444f9b452aa2
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2021-02-10 04:07:58 +0300

    60%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (count < 0) return false;
+      if (str[i] === bracketsConfig[j][0]) {
+        count += 1;
+      } else if (str[i] === bracketsConfig[j][1]) {
+        count -= 1;
+      }
+    }
+  }
+  return count === 0;
 }
